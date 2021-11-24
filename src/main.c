3722696af211776e720657691196be8998edd9be
Victor <v.minon@icloud.com>
2021-11-24 10:36:40 +0100

    GitIgnore+Doc

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/**
+ * @file main.c
+ * @author KVEP - https://gitlab.com/kvep/
+ * @version 1.0
+ * @date 2021-11-24
+ * 
+ * @copyright Copyright (c) 2021
+ */
+
+#include <stdio.h>
+
+
+int main()
+{
+	printf("Hello World!\n");
+
+	return (0);
+}
