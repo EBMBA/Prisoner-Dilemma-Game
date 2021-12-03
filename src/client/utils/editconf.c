dc43cbbdbec110aee840f12035d564870c8fc7ef
Victor <v.minon@icloud.com>
2021-12-03 11:59:11 +0100

    .id

--- src/client/utils/editconf.c
+++ src/client/utils/editconf.c
@@ -12,40 +12,35 @@
 #include <string.h>
 #include <errno.h>
 #include"ini.h"
-
-#define CONF_FILE "../config.ini"
-
 #include "editconf.h"
 
+#define CONF_FILE "../configuration/config.ini"
 
 
 void main()
 {
-    // char *file = "../config.ini";
+    idClient();
+    // // char *file = "../config.ini";
     // char *section = "Serveur Configuration";
-    // char *field = "IP";
-
-    // char *IP = readFiletest(file, section, field);
+    // //char *field = "IP";
+    
+    // char *IP = NULL ;
+    // IP = malloc(50 * sizeof(char));
+    // readFile(CONF_FILE, section, "IP", &IP);
     // printf("IP : %s \n",IP);
 
-    
-    
-    char *IP = NULL ;
-    IP = malloc(50 * sizeof(char));
-    readFiletest(CONF_FILE, "Serveur Configuration", "IP", &IP);
-    printf("IP : %s \n",IP);
+    // char *port= NULL;
+    // port = malloc(50 * sizeof(char));
+    // readFile(CONF_FILE, section, "port", &port);
+    // printf("port : %s \n",port);
 
-    char *port= NULL;
-    port = malloc(50 * sizeof(char));
-    readFiletest(CONF_FILE, "Serveur Configuration", "port", &port);
-    printf("port : %s \n",port);
+    // char *test = NULL;
+    // test =  malloc(50 * sizeof(char));
+    // readFile(CONF_FILE, section, "test", &test);
+    // printf("test : %s \n",test);
 
-    char *test = NULL;
-    test =  malloc(50 * sizeof(char));
-    readFiletest(CONF_FILE, "Serveur Configuration", "test", &test);
-    printf("test : %s \n",test);
+
 }
-
 
 /**
  * @brief return a param from a file location, a section, and a field
@@ -54,7 +49,7 @@ void main()
  * @param field 
  * @return char* : param in a config file .ini
  */
-void readFiletest(char *file, char *section, char *field, char **param)
+void readFile(char *file, char *section, char *field, char **param)
 {
     ini_t *f = ini_load(file);
     //const char *param ;
@@ -62,11 +57,52 @@ void readFiletest(char *file, char *section, char *field, char **param)
 
     if ((*param = ini_get(f, section, field)) != 0)
     {
+        ini_free(f);
         return;
     }
-    printf("Error : the param '%s' of the configuration file is not correct \n",field);
-    // perror("Configuration file is not correct"); ==> Pb SUCCESS apres le message 
+    // printf("Error : the param '%s' of the configuration file is not correct \n",field);
+    ini_free(f);
+    errno=61;
+    perror("Configuration file is not correct ");
     exit(-5);
+}
 
+
+void idClient()
+{
+    int id;
+    char text[128];
+    char tmpc[2];
+
+    FILE * fp;
+    fp = fopen("../configuration/.id","rb");
+
+    if (fp == NULL) {
+        errno=2;
+        perror("file '.id' is missing ");
+        exit(-5);
+    }
+
+    else
+    {
+        char c = fgetc(fp);
+        text[0]=c;
+        tmpc[1]=0;
+
+        while ((c != EOF)){
+            c = fgetc(fp);
+            tmpc[0]=c;
+            strcat(text, tmpc);
+        }
+        id = atoi(text);
+    }
+    if (id == 0)
+    {
+        printf("ID == 0\n");
+    }
+    else
+    {
+        printf("ID = %d\n",id);
+    }
 
 }
--- src/client/utils/editconf.h
+++ src/client/utils/editconf.h
@@ -1,1 +1,3 @@
-void readFiletest(char *file, char *section, char *field, char **param);
+void readFile(char *file, char *section, char *field, char **param);
+int digit_to_int(char d);
+void idClient();
