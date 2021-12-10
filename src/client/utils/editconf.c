52b6078633f94c7144e96f74618400ddcbf3f745
Victor <v.minon@icloud.com>
2021-12-10 09:58:21 +0100

    Getter Setter ID

--- src/client/utils/editconf.c
+++ src/client/utils/editconf.c
@@ -15,11 +15,20 @@
 #include "editconf.h"
 
 #define CONF_FILE "../configuration/config.ini"
+#define ID_FILE "../configuration/.id"
 
 
 void main()
 {
-    idClient();
+    // u_int16_t id = getID();
+    // printf("ID = %d\n",id);
+
+    // int verif=setID(16);
+    // printf("error code = %d\n",verif);
+
+    // id = getID();
+    // printf("ID = %d\n",id);
+
     // // char *file = "../config.ini";
     // char *section = "Serveur Configuration";
     // //char *field = "IP";
@@ -38,8 +47,6 @@ void main()
     // test =  malloc(50 * sizeof(char));
     // readFile(CONF_FILE, section, "test", &test);
     // printf("test : %s \n",test);
-
-
 }
 
 /**
@@ -52,15 +59,12 @@ void main()
 void readFile(char *file, char *section, char *field, char **param)
 {
     ini_t *f = ini_load(file);
-    //const char *param ;
-    // printf("Size of %d\n", sizeof(param));
 
     if ((*param = ini_get(f, section, field)) != 0)
     {
         ini_free(f);
         return;
     }
-    // printf("Error : the param '%s' of the configuration file is not correct \n",field);
     ini_free(f);
     errno=61;
     perror("Configuration file is not correct ");
@@ -68,16 +72,20 @@ void readFile(char *file, char *section, char *field, char **param)
 }
 
 
-void idClient()
+/**
+ * @brief get ID from ID_FILE
+ * @return u_int16_t : ID 
+ */
+u_int16_t getID()
 {
-    int id;
+    u_int16_t id;
     char text[128];
     char tmpc[2];
 
     FILE * fp;
-    fp = fopen("../configuration/.id","rb");
-
-    if (fp == NULL) {
+    fp = fopen(ID_FILE,"r");
+    if (fp == NULL) 
+    {
         errno=2;
         perror("file '.id' is missing ");
         exit(-5);
@@ -97,19 +105,35 @@ void idClient()
         id = atoi(text);
     }
     fclose(fp);
+    return id;
+}
 
-    FILE * fp2;
-    fp2 = fopen("../configuration/.id","wb");
-
-
-    if (id == 0)
+/**
+ * @brief set ID in ID_FILE
+ * @param ID value to write in the ID_FILE
+ * @return int : error code
+ */
+int setID(u_int16_t ID)
+{
+    int ret = 0;
+    FILE * fp;
+    fp = fopen(ID_FILE,"w");
+    if (fp == NULL) 
     {
-        printf("ID == 0\n"); //request ID
-
+        errno=2;
+        perror("file '.id' is missing ");
+        exit(-5);
     }
     else
     {
-        printf("ID = %d\n",id); //send ID to server
-    }
+        fprintf(fp, "%d",ID);
+        fclose(fp);
 
+        u_int16_t verif = getID();
+        if (ID != verif)
+        {
+            ret = -1;
+        }
+    }
+    return ret;
 }
--- src/client/utils/editconf.h
+++ src/client/utils/editconf.h
@@ -1,3 +1,4 @@
 void readFile(char *file, char *section, char *field, char **param);
 int digit_to_int(char d);
-void idClient();
+u_int16_t getID();
+int setID(u_int16_t ID);
