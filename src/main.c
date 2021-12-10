65e2ad953d5e8f63eb28eff67818e88d427d2002
Kaplan <kubilay.kpl@gmail.com>
2021-12-10 09:01:30 +0100

    update struct

--- src/main.c
+++ src/main.c
@@ -1,18 +1,18 @@
 /**
  * @file main.c
- * @author KVEP - https://gitlab.com/kvep/
+ * @author KVEP - https://gitlab.com/kvep
  * @version 1.0
- * @date 2021-11-24
+ * @date 2021-11-17
  * 
  * @copyright Copyright (c) 2021
  */
 
 #include <stdio.h>
+#include "../src/launcher.h"
 
-
-int main()
+int main(int argc, char **argv)
 {
-	printf("Hello World!\n");
-
+	init_main_window(argc,argv);
+	gtk_main();
 	return (0);
 }
