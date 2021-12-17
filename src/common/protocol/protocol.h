dcab756bb9f83c1466e27308514973f25d9e3cfa
Emile Metral <metral4269@gmail.com>
2021-12-17 10:44:40 +0000

    Update src/common/protocol/protocol.h, src/common/protocol/protocol.c

--- src/common/protocol/protocol.h
+++ src/common/protocol/protocol.h
@@ -12,8 +12,9 @@ typedef struct
     u_int16_t game_id;
     u_int16_t action_id; // wait == 0 ; play == 1 ; coop == 2 ; betray == 3 ; no reponse == 4 ; finish == 5;
     u_int16_t result_id; // Win or lose
-    u_int16_t money;
-    u_int16_t numberRound;
+    u_int16_t earned_money;
+    u_int16_t current_round;
+    u_int16_t time;
 } packet ;
 
 packet get_parse(char *bufferIn);
