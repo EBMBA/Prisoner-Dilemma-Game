9628ae31b2a5d4a86c20847345d1a59800ab7219
Victor <v.minon@icloud.com>
2021-12-19 11:44:26 +0100

    Mofif IP and Port for connection to the server

--- src/client/main.c
+++ src/client/main.c
@@ -15,8 +15,9 @@
 #include <unistd.h>
 #include <string.h>
 #include "../common/protocol/protocol.h"
+#include "../src/common/utils/editconf.h"
 
-#define PORT 8085
+//#define PORT 8085
 #define MAX_SOCK_SIZE 1024
 
 int main(int argc, char const *argv[])
@@ -37,7 +38,18 @@ int main(int argc, char const *argv[])
     packetd.money = 0;
     packetd.numberRound = 0;
 
-	//hello = malloc(100 * sizeof(char));
+	//Get info from confile
+    char *IP = NULL;
+    IP = malloc(50 * sizeof(char));
+    readFile(CLIENT_CONF_FILE, "Serveur Configuration", "IP", &IP);
+    //printf("%s\n", IP);
+
+    char *port = NULL;
+    port = malloc(50 * sizeof(char));
+    readFile(CLIENT_CONF_FILE, "Serveur Configuration", "port", &port);
+    //printf("%s\n", port);
+
+	hello = malloc(100 * sizeof(char));
 
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
@@ -46,10 +58,10 @@ int main(int argc, char const *argv[])
 	}
 
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(PORT);
+	serv_addr.sin_port = htons(port);
 	
 	// Convert IPv4 and IPv6 addresses from text to binary form
-	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+	if(inet_pton(AF_INET, IP, &serv_addr.sin_addr)<=0)
 	{
 		printf("\nInvalid address/ Address not supported \n");
 		return -1;
